75ec44d2e05e7c7ec47790c644f760ed49a4dc2d
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-13 18:42:43 +0530

    Code Updated

--- SumOfDigits/Program.cs
+++ SumOfDigits/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //Object creation to access the method
             SumOfDigitOfNum obj=new SumOfDigitOfNum();
             obj.Sum();
             
